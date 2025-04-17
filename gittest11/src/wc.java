caa3294cc6cdc83555d480d8d812d3a3a26f3e2b
kraws1 <2926732262@qq.com>
2025-04-17 12:18:16 +0800

    Update wc.java
    web modify

--- gittest11/src/wc.java
+++ gittest11/src/wc.java
@@ -2,5 +2,6 @@ public class wc {
     public static void main(String[] args) {
         System.out.println("WorldCount");
         System.out.println("hello github");
+        System.out.println("hello github web");
     }
 }
